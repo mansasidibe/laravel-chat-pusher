369e53f0719cfd251450d77dea1f7fc5d2d036ee
ronphobos <sidibearouna71@gmail.com>
2022-04-15 12:30:21 +0000

    ajout fonctions controller

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatsController extends Controller
 {
-    //
+    // PROTEGEONS TOUTES NOS FONCTION
+    public function __construct()
+    {
+    $this->middleware('auth');
+    }
+
+    public function index()
+    {
+    return view('chat');
+    }
+
+
+    public function fetchMessages()
+    {
+    return Message::with('user')->get();
+    }
+
+    public function sendMessage(Request $request)
+    {
+    $user = Auth::user();
+
+    $message = $user->messages()->create([
+        'message' => $request->input('message')
+    ]);
+
+    return ['status' => 'Message envoyé!'];
+    }
 }
